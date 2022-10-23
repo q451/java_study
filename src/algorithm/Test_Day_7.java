a8c436736968b3f5ccca850e8278a241a2c4f72b
YangXiaoping <1393371859@qq.com>
2022-10-23 13:52:57 +0800

    java日常练习

--- src/algorithm/Test_Day_7.java
+++ src/algorithm/Test_Day_7.java
@@ -1,17 +1,25 @@
 package algorithm;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Test_Day_7 {
     public static void main(String[] args) {
-        String s1 = "words and 987";
-        String s = s1.replaceAll(" ","");
-        char []a = s.toCharArray();
-        for(int i =0;i<a.length;i++){
-            if(Character.isLetter(a[i])){
-                a[i] = ' ';
+        int []nums = {0,1,0,13,12};
+        int index = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
+            }else{
+                count ++;
             }
         }
-        String s2 = new String(a);
-        String s3 = s2.replaceAll(" ","");
-        int num = Integer.parseInt(s3);
-        System.out.println(num);
+
+        for(int j = nums.length;j>nums.length -count;j--){
+            nums[j-1] = 0;
+        }
+        System.out.println(nums);
     }
 }
---
+++ src/algorithm/Test_Day_8.java
@@ -0,0 +1,8 @@
+package algorithm;
+
+public class Test_Day_8 {
+    public static void main(String[] args) {
+
+        System.out.println(111);
+    }
+}
